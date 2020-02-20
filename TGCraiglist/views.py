fa577c182f5779feaeb63bc4295d081edd0999ce
yodatk <37527725+yodatk@users.noreply.github.com>
2020-02-20 10:31:08 +0200

    adding images and connecting search capabilities to website

--- TGCraiglist/views.py
+++ TGCraiglist/views.py
@@ -6,6 +6,8 @@ from . import models
 
 BASE_CRAIGLIST_URL = "https://sfbay.craigslist.org/search/?query={}"
 
+BASE_IMG_URL = "https://images.craigslist.org/{}_300x300.jpg"
+
 
 # Create your views here.
 def home(request):
@@ -18,7 +20,28 @@ def new_search(request):
     final_url = BASE_CRAIGLIST_URL.format(quote_plus(search))
     response = requests.get(final_url)
     data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMG_URL.format(post_image_id)
+        else:
+            post_image_url = 'https://craiglist.org/images/peace.jpg'
+        final_postings.append((post_title, post_url, post_price,post_image_url))
+
     stuff_for_front_end = {
         'search': search,
+        'final_postings': final_postings,
     }
-    return render(request, 'my_app/new_search.html', stuff_for_front_end)
+    return render(request, 'TGCraiglist/new_search.html', stuff_for_front_end)
